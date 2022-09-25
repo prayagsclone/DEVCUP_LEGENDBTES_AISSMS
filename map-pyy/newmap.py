4f85fabdf3e1acf9d9c0ebe0e9495fa320417dfe
prayagsclone <prayagbhosale228008@gmail.com>
2022-09-26 00:14:29 +0530

    UPDATED the python model file ~pratham 12:12 PMj

--- map-pyy/newmap.py
+++ map-pyy/newmap.py
@@ -32,14 +32,70 @@ tooltip = {
     "html": "<b>{district},{severity}</b>",
     "style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000"},
 }
+
+#from cgitb import html
+#from ctypes.wintypes import RGB
+#from turtle import color
+import streamlit as st
+import pydeck as pdk
+import pandas as pd
+import math
+#from io import BytesIO
+
+
+
+df = pd.read_csv("use.csv")
+
+layer = pdk.Layer(
+    "ScatterplotLayer",
+    df,
+    pickable=True,
+    opacity=0.8,
+    stroked=True,
+    filled=True,
+    radius_scale=600,
+    radius_min_pixels=5,
+    radius_max_pixels=15,
+    line_width_min_pixels=1,
+    get_position=['long', 'lat'],
+    #get_radius="exits_radius",
+    get_fill_color=[255, 140, 0],
+    get_line_color=[0, 0, 0],
+    
+)
+tooltip = {
+    "html": "<b>{district},{severity}</b>",
+    "style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000"},
+}
 # Set the viewport location
-view_state = pdk.ViewState(longitude=78.348516,
-    latitude=22.824289, zoom=10, bearing=0, pitch=0)
+
+st.header('3D Visualization')
+
+#a=st.pydeck_chart
+#b=
+
+data=pd.read_csv('Indian_earthquake_data.csv')
+
+#layers setting
+layers=pdk.Layer('HexagonLayer',
+    data,
+    get_position=['Longitude', 'Latitude'],
+    auto_highlight=True,
+    elevation_scale=50,
+    pickable=True,
+    elevation_range=[0, 3000],
+    extruded=True,                 
+    coverage=1)
+
+#view Port Location
+view_state=pdk.ViewState(longitude=78.348516,
+    latitude=22.824289,
+    zoom=6,
+    min_zoom=1,
+    max_zoom=15,
+    pitch=40.5,
+    bearing=-27.36)
 
 # Render
-r = pdk.Deck(layers=[layer],tooltip=tooltip,initial_view_state=view_state)
-#r.to_html("scatterplot_layer.html")
+r = pdk.Deck(layers=[layers], initial_view_state=view_state)
 st.write(r)
-
-
-#st.download_button("map_offline", data=html, file_name="of_ma.html")
