a5f704c99c738016bf0702102ed4e2f18f5337fa
prayagsclone <prayagbhosale228008@gmail.com>
2022-09-27 00:17:22 +0530

    added datapoints on map for earthq and flood using js with different location of india and working of drop down of user selection and data transfer flow properly ~prayag 12:18 AM

--- public/client.js
+++ public/client.js
@@ -1,10 +1,25 @@
 import * as THREE from '/build/three.module.js';
 import Stats from './jsm/libs/stats.module.js';
 import { OrbitControls } from './jsm/controls/OrbitControls.js';
+import { Scene } from 'three';
 console.log(THREE);
 console.log(OrbitControls);
 
 const canvas = document.querySelector('.web-gl');
+
+// Globe Dimensions
+
+const el = document.getElementById('box');
+console.log(el);
+const gW = el.offsetWidth;
+const gH = el.offsetHeight;
+console.log(gW, gH);
+
+
+// const boxG = document.getElementById('.globeIDG');
+// let globe_width = boxG.offsetWidth;
+// let globe_height = boxG.offsetHeight;
+// console.log(boxG.offsetWidth, boxG.offsetHeight);
 
 // showing fps
 const stats = new Stats();
@@ -15,7 +30,7 @@ const scene = new THREE.Scene();
 console.log(scene);
 
 // Camera Setup
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(75, gW / gH, 0.1, 1000)
 camera.position.z = 12;
 scene.add(camera);
 console.log(camera);
@@ -26,15 +41,16 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
 });
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(gW, gH);
 renderer.setPixelRatio((window.devicePixelRatio) ? window.devicePixelRatio : 1);
+renderer.setClearColor(0xffffff, 0);
 
 //earth
 const earth = new THREE.Mesh(
-    new THREE.SphereGeometry(5, 50, 50),
+    new THREE.SphereGeometry(7.3, 50, 50),
     new THREE.MeshBasicMaterial({
         map: new THREE.TextureLoader().load('./texture/earth.png')
-    })
+    }),
 )
 
 scene.add(earth);
@@ -52,7 +68,7 @@ const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    earth.rotation.y = .2 * elapsedTime
+    earth.rotation.y = .1 * elapsedTime
 
     render();
 
@@ -64,10 +80,18 @@ tick()
 
 // Resizing window to make responsive
 const windowResize = () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.aspect = gW / gH;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(gW / 2, gH / 2);
     render();
 }
 
 window.addEventListener('resize', windowResize, false);
+
+
+// quaternion = new THREE.Quaternion().setFromAxisAngle(100, 10);
+// earth.rotation.set(new THREE.Euler().setFromQuaternion(quaternion));
+
+
+
+
--- users.js
+++ users.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
 
 let userSchema = new mongoose.Schema({
-    _id:mongoose.Schema.Types.ObjectId,
+    _id: mongoose.Schema.Types.ObjectId,
     email_id: String,
     password: String,
-    reg_name:String,
-    city:String,
-    state:String,
+    reg_name: String,
+    city: String,
+    state: String,
     map_data: String,
     charts_data: String,
-    cala_n:   [String],
+    cala_n: [String],
     chart_lab: String,
-    slider: String
+    slider: array,
+    code: String,
+    data_location: String
 
 })
 
-module.exports = mongoose.model("users",userSchema);
+module.exports = mongoose.model("users", userSchema);
